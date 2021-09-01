d187bad7c8bc533d4a3e90d20783520b75527f58
Franklin van Nes <franklin@lastmileretail.com>
2021-09-01 14:44:13 -0400

    Settings documentation and example dotenv file.

--- api/settings.py
+++ api/settings.py
@@ -1,6 +1,8 @@
 from pydantic.env_settings import BaseSettings
 
-
+# Fields of this class are settings parsed by Pydantic from the .env file in your
+# environment, and also from your environment variables. Environment variables take precedence.
+# See https://pydantic-docs.helpmanual.io/usage/settings/#parsing-environment-variable-values
 class Settings(BaseSettings):
     database_url: str
 
